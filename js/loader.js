71f098a4c664df612590ec0c551d35fc2c1cbb96
Marius T <marius.timisescu@gmail.com>
2019-02-07 21:46:53 +0200

    btn event listener

--- js/loader.js
+++ js/loader.js
@@ -1,7 +1,11 @@
-var request = new XMLHttpRequest();
-request.open("GET", "data/players.json");
-request.onload = function() {
-    var players = JSON.parse(request.responseText);
-    console.log(players);
-}
-request.send();
+var btn = document.getElementById("btn");
+btn.addEventListener("click", function(){
+    var request = new XMLHttpRequest();
+    request.open("GET", "data/players.json");
+    request.onload = function() {
+        var players = JSON.parse(request.responseText);
+        console.log(players);
+    }
+    request.send();
+});
+
