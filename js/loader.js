49e6a4da200f94e290459517b60f2bfb3310a6a6
Marius T <marius.timisescu@gmail.com>
2019-02-07 21:55:06 +0200

    render text

--- js/loader.js
+++ js/loader.js
@@ -1,11 +1,18 @@
 var btn = document.getElementById("btn");
+var info = document.getElementById("info");
+
 btn.addEventListener("click", function(){
     var request = new XMLHttpRequest();
     request.open("GET", "data/players.json");
     request.onload = function() {
         var players = JSON.parse(request.responseText);
-        console.log(players);
+        render(players);
     }
     request.send();
 });
+
+function render(data) {
+    info.insertAdjacentHTML('beforeend', 'test123');
+    console.log(players);
+}
 
