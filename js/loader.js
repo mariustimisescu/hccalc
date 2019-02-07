db3bc5ffe0e0ec48df0ec2e94bcea73ba9ed122d
Marius T <marius.timisescu@gmail.com>
2019-02-07 21:57:01 +0200

    js spacing

--- js/loader.js
+++ js/loader.js
@@ -1,10 +1,10 @@
 var btn = document.getElementById("btn");
 var info = document.getElementById("info");
 
-btn.addEventListener("click", function(){
+btn.addEventListener("click", function () {
     var request = new XMLHttpRequest();
     request.open("GET", "data/players.json");
-    request.onload = function() {
+    request.onload = function () {
         var players = JSON.parse(request.responseText);
         render(players);
     }
