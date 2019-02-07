efd9ca58f3b8b6c206d90c4121f82a32e5bc3dca
Marius T <marius.timisescu@gmail.com>
2019-02-07 19:37:47 +0200

    commit

--- demo.js
+++ demo.js
@@ -1,3 +1,3 @@
-$().ready(function() {
+$().ready(function () {
    $("#text").html("Text added by jQuery code.");
 });
--- json.js
+++ json.js
@@ -1,5 +1,5 @@
-$().ready(function(){
-    $.getJSON( "/data.json", function( data ) {
+$().ready(function () {
+  $.getJSON("/data.json", function (data) {
     console.log(data);
     $("#text").html(data["text"]);
   });
