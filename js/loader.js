a047471d495d2a56b4936d2a3eaaad2e28656e71
Marius T <marius.timisescu@gmail.com>
2019-02-08 19:28:03 +0200

    added players page

--- js/loader.js
+++ js/loader.js
@@ -1,12 +1,14 @@
 var allPlayers = [];
 var allTowers = [];
+var towerNo = 0;
 
 window.onload = function () {
-    var btn = document.getElementById("btn");
-    btn.addEventListener("click", onClick);
+    var btnNewWar = document.getElementById("btnNewWar");
+    btnNewWar.addEventListener("click", onClickNewWar);
+    var btnAddTower = document.getElementById("btnAddTower");
+    btnAddTower.addEventListener("click", onClickAddTower);
     loadPlayers();
     loadTowers();
-    fillAvailablePlayersColumn(allPlayers);
 }
 
 function loadPlayers() {
@@ -31,8 +33,42 @@ function loadTowers() {
     request.send();
 }
 
-function onClick() {
+function onClickNewWar() {
+    fillAvailablePlayersColumn(allPlayers);
+
     console.log(allPlayers);
+}
+
+function onClickAddTower() {
+    towerNo++;
+    var towerConfigurationRow = document.getElementById("towerConfigurationRow");
+
+    var div = document.createElement("div");
+    div.setAttribute("class","column");
+    towerConfigurationRow.appendChild(div);
+
+    var ul = document.createElement("ul");
+    ul.setAttribute("class", "connected-sortable droppable-area2");
+    ul.setAttribute("id", "tower" + towerNo);
+
+    var towerText = document.createTextNode("Turn "+towerNo);
+    ul.appendChild(towerText);
+    div.appendChild(ul);
+
+    console.log(allTowers);
+}
+
+function simplifySP(sp) {
+    var result = "";
+    var spInt = parseInt(sp);
+    if (spInt > 1000000) {
+        result = (parseFloat(spInt / 1000000).toFixed(3)).toString() + "m";
+    } else if (spInt > 9999) {
+        result = (parseFloat(spInt / 1000).toFixed(0)).toString() + "k";
+    } else {
+        result = spInt.ToString();
+    }
+    return result;
 }
 
 function fillAvailablePlayersColumn(data) {
@@ -42,7 +78,12 @@ function fillAvailablePlayersColumn(data) {
         var li = document.createElement("li");
         li.setAttribute("id", "player" + pi.id);
         li.setAttribute("class", "draggable-item");
-        li.appendChild(document.createTextNode(pi.playerName));
+        var playerText = document.createTextNode(pi.playerName);
+        li.appendChild(playerText);
+        playerText = document.createElement("br");
+        li.appendChild(playerText);
+        playerText = document.createTextNode("TR" + pi.throneRoom + " " + simplifySP(pi.squadPower));
+        li.appendChild(playerText);
         ul.appendChild(li);
     };
     //console.log(data);
