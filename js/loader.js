34c961bd3fb56c1f85ded2f7e420105a970437d9
Marius T <marius.timisescu@gmail.com>
2019-02-08 18:14:06 +0200

    changes

--- js/loader.js
+++ js/loader.js
@@ -1,18 +1,50 @@
-var btn = document.getElementById('btn');
-var infoArea = document.getElementById('info');
+var allPlayers = [];
+var allTowers = [];
 
-btn.addEventListener("click", function () {
+window.onload = function () {
+    var btn = document.getElementById("btn");
+    btn.addEventListener("click", onClick);
+}
+
+function loadDataFromJson() {
     var request = new XMLHttpRequest();
     request.open("GET", "data/players.json");
     request.onload = function () {
-        var players = JSON.parse(request.responseText);
-        render(players);
+        allPlayers = JSON.parse(request.responseText);
     }
     request.send();
-});
 
-function render(data) {
-    infoArea.insertAdjacentHTML('beforeend', 'test123');
-    console.log(players);
+    request.open("GET", "data/towers.json");
+    request.onload = function () {
+        allTowers = JSON.parse(request.responseText);
+    }
+    request.send();
 }
 
+function onClick() {
+
+    //console.log(players);
+    loadAllPlayers(allPlayers);
+
+}
+
+function loadAllPlayers(data) {
+    for (var i = 0; i < data.length; i++) {
+        var pi = data[i];
+        addAvailablePlayer(pi);
+    };
+    //console.log(data);
+};
+
+function addAvailablePlayer(pi) {
+    var ul = document.getElementById("availablePlayers");
+    var li = document.createElement("li");
+    li.setAttribute("id", "player" + pi.id);
+    li.setAttribute("class", "draggable-item");
+    li.appendChild(document.createTextNode(pi.playerName));
+    ul.appendChild(li);
+}
+
+function loadAllTowers() {
+
+}
